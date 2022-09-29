18ea55315aa780a4c3a07243209ea263316e1b20
BrownMark <mark.brown@unosquare.com>
2022-09-29 11:34:41 +0100

    Test database CICD

--- .github/workflows/database/V1.0__initial_schema_setup.sql
+++ .github/workflows/database/V1.0__initial_schema_setup.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS public.test
     id serial constraint test_pk primary key,
     description  text
 );
+
+
+CREATE TABLE IF NOT EXISTS public.testtwo
+(
+    id serial constraint test_two_pk primary key,
+    description  text
+);
